d2778b35ab5e4a28596b09d627164f7b4f87cc42
wanfrev <wanfrevargas@gmail.com>
2026-03-26 21:47:08 -0400

    cambios

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      screens: {
+        desktop: '1440px',
+      },
       colors: {
         'dark-bg': '#0b0118',
         'trueview-blue': '#2563eb',
